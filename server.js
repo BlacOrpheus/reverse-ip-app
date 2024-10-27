0d16e017288fb8ae12e24ee3e25b7b13f9274db0
BlacOrpheus <chidubemchinwuba01@gmail.com>
2024-10-27 19:39:36 +0100

    evening push1

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-//const cors = require('cors');
 const path = require('path');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
@@ -8,16 +7,12 @@ const app = express();
 const port = 3000;
 
 // Middleware
-//app.use(cors());
 app.use(express.static(path.join(__dirname)));
-
 
 // Root route to serve the HTML file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-
 
 // MongoDB connection URI
 const uri = process.env.MONGODB_URI;
@@ -45,38 +40,28 @@ async function connectToDatabase() {
 connectToDatabase();
 
 // Route to get and reverse the client's IP
-
-  
 app.get('/get-reverse-ip', async (req, res) => {
-try {
-    // retrieve ip address
-const response = await fetch('https://api.ipify.org?format=json') 
-const data = await response.json()
+  try {
+    // Retrieve the client's IP address from the X-Forwarded-For header
+    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log("Client IP:", clientIp);
 
-if(!data.ip)return
+    // Perform reversal
+    const reversedIp = clientIp.split('.').reverse().join('.');
 
+    // Update database
+    const db = client.db('ipDatabase');
+    const collection = db.collection('ipAddresses');
+    await collection.insertOne({ ip: reversedIp });
+    res.status(200).json({ data: { reversedIp } });
 
-//perform reversal
-const reversedIp = data.ip.split('.').reverse().join('.');
-
-//update database
-const db = client.db('ipDatabase');
-const collection = db.collection('ipAddresses');
-await collection.insertOne({ ip: reversedIp });
-res.status(200).json({data:{reversedIp}})
-
-
-
- } catch (error) {
-    res.status(500).json({message:error})
- }
-
+  } catch (error) {
+    console.error("Error processing request:", error);
+    res.status(500).json({ message: error.message });
+  }
 });
-
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-
